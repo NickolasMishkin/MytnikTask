7704f8c94060035287c26db2f1617c04d81438c2
Nikolai <kampotich2000@mail.ru>
2021-08-20 16:36:40 +0300

    Delete unused files

--- Source/TestTask/MenuCharacter.h
+++ Source/TestTask/MenuCharacter.h
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-#include "TT_SaveProgress.h"
 #include "TT_GameInstance.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
